c4226177af0a59e599748649bad5c66a2231996d
majdshatti <majdshatti8@gmail.com>
2022-09-04 01:55:11 +0300

    UserController init

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class UserController extends Controller
+{
+    //
+    public function getUsers(){
+
+    }
+}
