7ec4c551ba3038bdffbacf6ddd0fa792fca49e63
majdshatti <majdshatti8@gmail.com>
2022-09-05 02:49:10 +0300

    General improvement

--- app/Utility/mail/sendMail.php
+++ app/Utility/mail/sendMail.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Mail;
 
+/**
+ * Sends email using smtp
+ *
+ * @param string $mail    Destination mail that message is sent to.
+ * @param string $subject The subject of the sent message.
+ * @param string $body    The body of the sent message.
+ *
+ * @return boolean
+ */
 if (!function_exists("sendMail")) {
     function sendMail($email, $subject, $body)
     {
@@ -11,11 +20,17 @@ if (!function_exists("sendMail")) {
             "body" => $body,
         ];
 
-        Mail::send([], $data, function ($message) use ($data) {
-            $message->to($data["email"]);
-            $message->subject($data["subject"]);
-            $message->text($data["body"]);
-        });
+        try {
+            Mail::send([], $data, function ($message) use ($data) {
+                $message->to($data["email"]);
+                $message->subject($data["subject"]);
+                $message->text($data["body"]);
+            });
+
+            return true;
+        } catch (Exception $exception) {
+            return false;
+        }
     }
 }
 
