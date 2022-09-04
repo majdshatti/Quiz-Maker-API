fdc9fe1dc9e8b4e4f1a057c92a30882ac26600b8
majdshatti <majdshatti8@gmail.com>
2022-09-05 02:50:42 +0300

    Verifiy Account route added

--- routes/auth.php
+++ routes/auth.php
@@ -1,6 +1,14 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\VerifiyAccountController;
 use Illuminate\Support\Facades\Route;
 
+// Reigster a user
 Route::post("/register", [AuthController::class, "register"]);
+
+// Verify a user account
+Route::post("/verify/{token}", [
+    VerifiyAccountController::class,
+    "verifyAccount",
+]);
