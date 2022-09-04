44fa88f5033f825748e827eff27702b5c9a9ad75
majdshatti <majdshatti8@gmail.com>
2022-09-05 02:49:38 +0300

    General improvement

--- app/Utility/response/ErrorResponse.php
+++ app/Utility/response/ErrorResponse.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Returns error response to the client.
+ *
+ * @param Throwable $exception An instance of throwable exception class.
+ *
+ * @return JsonResponse
+ */
 if (!function_exists("errorResponse")) {
     function errorResponse($exception)
     {
@@ -13,31 +20,10 @@ if (!function_exists("errorResponse")) {
         // Response object
         $response = [];
         $response["success"] = false;
+        $response["message"] = $exception->getMessage();
 
-        // Check exception status and set response message
-        switch ($statusCode) {
-            case 401:
-                $response["message"] = getResMessage("401");
-                break;
-            case 403:
-                $response["message"] = getResMessage("403");
-                break;
-            case 404:
-                $response["message"] = getResMessage("404");
-                break;
-            case 405:
-                $response["message"] = getResMessage("405");
-                break;
-            case 422:
-                $response["message"] = getResMessage("422");
-                $response["errors"] = $exception->errors();
-                break;
-            default:
-                $response["message"] =
-                    $statusCode == 500
-                        ? getResMessage("500")
-                        : $exception->getMessage();
-                break;
+        if (method_exists($exception, "errors")) {
+            $response["errors"] = $exception->errors();
         }
 
         // Return json response
--- app/Utility/response/GetResMessage.php
+++ app/Utility/response/GetResMessage.php
@@ -1,21 +1,56 @@
 <?php
 
+/**
+ * Returns a message suitable for response, can be an error or success message.
+ *
+ * @param string $keyWord Key word that represents sentance in the switch statment.
+ *
+ * @return String message
+ */
 if (!function_exists("getResMessage")) {
-    function getResMessage(string $keyWord)
+    function getResMessage($keyWord, $options = [])
     {
+        $path = $options["path"] ?? ""; // Example: User, Quiz, Subject
+        $value = $options["value"] ?? ""; // Example: Majd, 23124, majd-nuzha
+        $field = $options["field"] ?? ""; // Example: name, createdAt, slug
+
         switch ($keyWord) {
-            case "401":
+            case "alreadyVerfied":
+                return "Account is already verified!";
+            case "authenticate":
+                return "Unauthenticated to access this route";
+            case "authorize":
                 return "Unauthorized to access this route";
-            case "403":
-                return "Forbbiden";
-            case "404":
-                return "Not found";
-            case "422":
+            case "badRequest":
                 return "Bad Request";
-            case "500":
-                return "Something went wrong :<, try again later";
+            case "created":
+                return ($path ? ucfirst($path) : "Record") .
+                    " is created successfully";
+            case "deleted":
+                return ($path ? ucfirst($path) : "Record") .
+                    " is deleted successfully";
+            case "edited":
+                return ($path ? ucfirst($path) : "Record") .
+                    " is edited successfully ";
+            case "forbbiden":
+                return "Forbbiden";
+            case "invalidToken":
+                return "Invailed token provided";
+            case "notExist":
+                return ($value ? ucfirst($value) : "Value") . " does not exist";
+            case "notFound":
+                return "Not found";
+            case "notUnique":
+                return ucfirst($path) .
+                    " with the same {$field} already exists";
             case "registered":
                 return "Account registered successfully";
+            case "serverError":
+                return "Something went wrong while trying to " .
+                    ($path ? strtolower($path) : "perform this operation") .
+                    ", if problem persists contact us at info@company.com";
+            case "verified":
+                return "Account verified successfully";
             default:
                 return false;
         }
