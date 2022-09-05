7901517e4be69505f9d7f4927a55b55fdd6bf26b
Abdul-Rahman Nuzha <abdalrahmannuzha@gmail.com>
2022-09-05 23:48:55 +0300

    edited use dir

--- routes/auth.php
+++ routes/auth.php
@@ -1,12 +1,17 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\VerifyAccountController;
+
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\VerifyAccountController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
 
 // Reigster a user
 Route::post("/register", [AuthController::class, "register"]);
+
 Route::post("/login", [AuthController::class, "login"]);
+
+Route::post("/password/email",  [ForgotPasswordController::class,"forgotPassword"]);
 
 Route::group(["middleware" => ["auth:sanctum"]], function () {
     Route::post("/logout", [AuthController::class, "logout"]);
