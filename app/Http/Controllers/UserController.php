14e38edd8a13b8e1e576a846cd445d34179a1e23
majdshatti <majdshatti8@gmail.com>
2022-09-12 00:19:33 +0300

    getUsers function added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,53 +3,61 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use App\Exceptions\ErrorResException;
 
 class UserController extends Controller
 {
-    //
-    public function getUsers(){
-
+    //* @desc: Get a list of users
+    //* @route: /api/user
+    //* @access: `ADMIN`
+    public function getUsers(Request $request)
+    {
+        // Return a list of users
+        return sendSuccRes([
+            "data" => User::sort($request)
+                ->filter(request(["name", "email", "rank"]))
+                ->simplePaginate($request->query("limit") ?? 10),
+        ]);
     }
+
     //* @desc:Change password for the user
     //* @route:/api/changePassword
     //* @access: PUBLIC
-    public function changePassword(Request $request ,$slug)
+    public function changePassword(Request $request, $slug)
     {
-
         $fields = $request->validate([
-            'oldpassword' =>'required|string',
-            'newpassword'=> ['required', 
-            'min:8', 
-            'regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%]).*$/',
-            'confirmed'] ,
-         ]);
-        
-         //Search for the user by slug to get th email.
-        $user = User::where('slug',$slug)->first();
-        
+            "oldpassword" => "required|string",
+            "newpassword" => [
+                "required",
+                "min:8",
+                'regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%]).*$/',
+                "confirmed",
+            ],
+        ]);
+
+        //Search for the user by slug to get th email.
+        $user = User::where("slug", $slug)->first();
+
         //Check the Email for the user if it is correct.
-        if(!$user)
-        {
-            throw new ErrorResException(getResMessage("notExist", ["value" => $slug]), 404);
+        if (!$user) {
+            throw new ErrorResException(
+                getResMessage("notExist", ["value" => $slug]),
+                404
+            );
         }
 
         //Check the Password for the user if it is correct.
-        if(!Hash::check($fields['oldpassword'],$user->password))
-        {
+        if (!Hash::check($fields["oldpassword"], $user->password)) {
             //respons message for old password
-            return getResMessage("notCorrect","Password");
+            return getResMessage("notCorrect", "Password");
         }
 
         //Update the password
-        User::where('slug',$slug)
-       ->update([
-           'password' => bcrypt($fields['newpassword'])
+        User::where("slug", $slug)->update([
+            "password" => bcrypt($fields["newpassword"]),
         ]);
-        return getResMessage("edited","Password");
+        return getResMessage("edited", "Password");
     }
-    
 }
