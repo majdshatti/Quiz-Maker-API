fc71a2f717632038cbdd33bd3932b606442f5ab4
Abdul-Rahman Nuzha <abdalrahmannuzha@gmail.com>
2022-09-12 18:06:55 +0300

    created and put getResMessages keys

---
+++ lang/en/resMessages.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    "alreadyVerfied" => "Account is already verified!",
+    "authenticate" => "Unauthenticated to access this route",
+    "authorize" => "Unauthorized to access this route",
+    "badRequest" => "Bad Request",
+    "codeSent" => "Password reset code is sent",
+    "created" => ":path is created successfully",
+    "creds" => "invalid creds",
+    "deleted" => ":path is deleted successfully",
+    "edited" => ":path is edited successfully",
+    "forbbiden" => "Forbbiden",
+    "invalidToken" => "Invalid token provided",
+    "logout" => "Logged out",
+    "notCorrect" => ":value does not correct",
+    "notExist" => ":value does not exist",
+    "notFound" => "Not found",
+    "notUnique" => ":path with the same :field already exists",
+    "registered" => "Account registered successfully",
+    "serverError" => "Something went wrong while trying to :path" . "perform this operation"
+        . ", if problem persists contact us at info@company.com",
+    "verified" => "Account verified successfully",
+
+
+
+
+];
