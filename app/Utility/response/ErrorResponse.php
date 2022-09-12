0125de6484712560fdfe12ae87be92a232fb8857
majdshatti <majdshatti8@gmail.com>
2022-09-12 23:46:46 +0300

    Added translators and improved responses

--- app/Utility/response/ErrorResponse.php
+++ app/Utility/response/ErrorResponse.php
@@ -8,11 +8,13 @@
  * @return JsonResponse
  */
 if (!function_exists("errorResponse")) {
-    function errorResponse($exception)
+    function errorResponse($exception, string $customMessage = "")
     {
         // Check for a status code in the exception
         if (isset($exception->status)) {
             $statusCode = $exception->status;
+        } elseif ($exception->getCode()) {
+            $statusCode = $exception->getCode();
         } else {
             $statusCode = 500;
         }
@@ -20,7 +22,8 @@ if (!function_exists("errorResponse")) {
         // Response object
         $response = [];
         $response["success"] = false;
-        $response["message"] = $exception->getMessage();
+        $response["message"] =
+            $customMessage === "" ? $exception->getMessage() : $customMessage;
 
         if (method_exists($exception, "errors")) {
             $response["errors"] = $exception->errors();
