c9e400c4abb98279d3b52ea679f053f321b36159
Abdul-Rahman Nuzha <abdalrahmannuzha@gmail.com>
2022-09-13 00:15:29 +0300

    improvements

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Log;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use App\Exceptions\ErrorResException;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
@@ -53,12 +54,12 @@ class UserController extends Controller
     }
 
     //* @desc:Edit user information
-    //* @route:/user/{slug}/edituser
-    //* @access: PUBLIC
-    public function edituser(Request $request,$slug)
+    //* @route:PUT /user/{slug}/edituser
+    //* @access: PRIVATE
+    public function editUser(Request $request,string $lang,string $slug)
     {
         $fields = $request->validate([
-            "newname" =>"required|string"
+            "name" =>"required|string"
          ]);
          
         //Search for the user by slug.
@@ -70,24 +71,39 @@ class UserController extends Controller
             throw new ErrorResException(getResMessage("notExist", ["value" => $slug]), 404);
         }
 
+        $loggedUserSlug = auth()->user()->slug;
+        //checking if the logged user slug equal to the slug sent by request
+        if($loggedUserSlug != $slug)
+        {
+            throw new ErrorResException(getResMessage("authorize", ["value" => $slug]), 404);
+        }
+
         User::where('slug',$slug)
         ->update([
-            'name' => $fields['newname']
+            'name' => $fields['name'],
+            'slug' => Str::slug($fields['name']),
          ]);
 
          return getResMessage("edited",["path"=>"user"]);
     }
 
     //* @desc:Delete user
-    //* @route:/user/{slug}/deleteuser
-    //* @access: PUBLIC
-    public function deleteUser(Request $request,$slug)
+    //* @route:DELETE /user/{slug}/deleteuser
+    //* @access: PRIVATE
+    public function deleteUser(Request $request,string $lang,string $slug)
     {
         $user = User::where('slug',$slug)->first();
 
         if(!$user)
         {
             throw new ErrorResException(getResMessage("notExist", ["value" => $slug]), 404);
+        }
+
+        $loggedUserSlug = auth()->user()->slug;
+        //checking if the logged user slug equal to the slug sent by request
+        if($loggedUserSlug != $slug)
+        {
+            throw new ErrorResException(getResMessage("authorize", ["value" => $slug]), 404);
         }
         
         User::where('slug',$slug)
--- routes/user.php
+++ routes/user.php
@@ -8,7 +8,7 @@ Route::group(["middleware" => ["auth:sanctum"]], function () {
 
     Route::put("/user/{slug}/changepassword", [UserController::class, "changePassword"]);
     
-    Route::put("/user/{slug}", [UserController::class, "edituser"]);
+    Route::put("/user/{slug}", [UserController::class, "editUser"]);
 
     Route::delete("/user/{slug}", [UserController::class, "deleteUser"]);
 
