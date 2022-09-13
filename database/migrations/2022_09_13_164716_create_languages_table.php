63b77646e287163288a06255aba49f7b23f6c45f
majdshatti <majdshatti8@gmail.com>
2022-09-13 20:10:22 +0300

    Lang table added and uuid updated

--- database/migrations/2022_09_13_164716_create_languages_table.php
+++ database/migrations/2022_09_13_164716_create_languages_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Str;
 
 return new class extends Migration {
     /**
@@ -13,16 +12,11 @@ return new class extends Migration {
      */
     public function up()
     {
-        Schema::create("user", function (Blueprint $table) {
+        Schema::create("language", function (Blueprint $table) {
             $table->id();
             $table->uuid("uuid");
             $table->string("name");
-            $table->string("slug");
-            $table->string("email")->unique();
-            $table->timestamp("email_verified_at")->nullable();
-            $table->string("password");
-            $table->integer("rank")->nullable();
-            $table->rememberToken();
+            $table->string("code");
             $table->timestamps();
         });
     }
@@ -34,6 +28,6 @@ return new class extends Migration {
      */
     public function down()
     {
-        Schema::dropIfExists("users");
+        Schema::dropIfExists("languages");
     }
 };
