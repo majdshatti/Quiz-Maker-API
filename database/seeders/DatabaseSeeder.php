e5c4300990418c7e3e269b0262925b983252068e
majdshatti <majdshatti8@gmail.com>
2022-09-15 23:43:31 +0300

    added quiz subject seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ use App\Models\SubjectTranslation;
 use App\Models\User;
 use Faker\Factory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 
@@ -65,8 +67,70 @@ class DatabaseSeeder extends Seeder
             ],
         ]);
 
-        Quiz::factory(10)->create();
-        
-        QuizTranslation::factory(10)->create();
+        //***********************/
+        //******* SUBJECT *******/
+        //***********************/
+        $subjectJson = File::get("database/data/subject.json");
+        $subjectTransJson = File::get("database/data/subjectTranslation.json");
+
+        $subjects = json_decode($subjectJson);
+        $subjectsTrans = json_decode($subjectTransJson);
+
+        foreach ($subjects as $key => $value) {
+            Subject::create([
+                "id" => $value->id,
+                "slug" => $value->slug,
+                "uuid" => $value->uuid,
+            ]);
+        }
+
+        foreach ($subjectsTrans as $key => $value) {
+            SubjectTranslation::create([
+                "id" => $value->id,
+                "uuid" => $value->uuid,
+                "language_id" => $value->language_id,
+                "subject_id" => $value->subject_id,
+                "name" => $value->name,
+                "description" => $value->description,
+            ]);
+        }
+
+        //***********************/
+        //******** QUIZ *********/
+        //***********************/
+        Quiz::insert([
+            [
+                "uuid" => Str::orderedUuid()->getHex(),
+                "slug" => "beginner",
+            ],
+            [
+                "uuid" => Str::orderedUuid()->getHex(),
+                "slug" => "intermediate",
+            ],
+            [
+                "uuid" => Str::orderedUuid()->getHex(),
+                "slug" => "advanced",
+            ],
+        ]);
+
+        //*******************************/
+        //******** SUBJECT QUIZ *********/
+        //*******************************/
+
+        $subjectQuizJson = File::get("database/data/quizSubject.json");
+
+        $subjectQuiz = json_decode($subjectQuizJson);
+
+        foreach ($subjectQuiz as $key => $value) {
+            DB::table("quiz_subject")->insert([
+                "id" => $value->id,
+                "quiz_id" => $value->quiz_id,
+                "subject_id" => $value->subject_id,
+            ]);
+        }
+
+        // Quiz::factory(10)->create();
+
+        // QuizTranslation::factory(10)->create();
     }
 }
