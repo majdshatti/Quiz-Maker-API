ad50ea5780793deaf19d5043f7f4206cbf092552
Abdul-Rahman Nuzha <abdalrahmannuzha@gmail.com>
2022-09-19 00:34:47 +0300

    created and added some rules

---
+++ app/Http/Requests/Answer/AnswerCreateRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\Answer;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Language;
+class AnswerCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        $langs = Language::all();
+
+        $rules = [
+             "questionId" => "required|string",
+             "isCorrect" => "required|boolean",
+
+    ];
+
+        foreach ($langs as $lang) {
+            $rules = array_merge($rules, [
+                $lang["code"] . ".name" => "required|string|unique:answer_translation,name",
+                $lang["code"] . ".paragraph" => "required|string|min:10|max:400",
+            ]);
+        }
+        return $rules;
+    }
+}
