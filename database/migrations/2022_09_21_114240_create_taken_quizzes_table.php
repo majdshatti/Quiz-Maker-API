7b15e5c594bbfa69f27aacec3fef30fd18274918
Abdul-Rahman Nuzha <abdalrahmannuzha@gmail.com>
2022-09-22 17:55:59 +0300

    created

---
+++ database/migrations/2022_09_21_114240_create_taken_quizzes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taken_quiz', function (Blueprint $table) {
+            $table->id();
+            $table->uuid("uuid");
+            $table->float("score");
+            $table->string("status");
+            $table->foreignId('subject_quiz_id')->constrained('quiz_subject')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('user')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taken_quiz');
+    }
+};
