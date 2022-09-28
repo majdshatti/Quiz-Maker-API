b5beea5ea02f257b4a6c8c692fa6d0f16f989b0d
majdshatti <majdshatti8@gmail.com>
2022-09-28 23:00:12 +0300

    feat: add route for submitting answer

--- routes/question.php
+++ routes/question.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\TakenQuizAnswerController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(["middleware" => ["auth:sanctum"]], function () {
@@ -10,11 +11,16 @@ Route::group(["middleware" => ["auth:sanctum"]], function () {
         //*********************************/
         Route::get("/", [QuestionController::class, "getQuestions"]);
         Route::get("/{uuid}", [QuestionController::class, "getQuestionByUuid"]);
+        Route::post("/{uuid}/answer", [
+            TakenQuizAnswerController::class,
+            "submitAnswer",
+        ]);
 
         Route::group(["middleware" => ["authorize"]], function () {
             //*********************************/
             //***** ADMIN PRIVATE ROUTES ******/
             //*********************************/
+
             Route::post("/", [QuestionController::class, "createQuestion"]);
             Route::put("/{uuid}", [QuestionController::class, "editQuestion"]);
             Route::delete("/{uuid}", [
