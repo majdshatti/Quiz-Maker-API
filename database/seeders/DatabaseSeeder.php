4a378e1304e522b90a13412d3e8686c2e6c734a9
majdshatti <majdshatti8@gmail.com>
2022-09-28 22:59:39 +0300

    refactor: all data is now seeded from json files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,20 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-use App\Models\Language;
-use App\Models\Question;
-use App\Models\QuestionTranslation;
-use App\Models\Quiz;
-use App\Models\QuizTranslation;
-use App\Models\Subject;
-use App\Models\SubjectTranslation;
-use App\Models\User;
-use Faker\Factory;
+use App\Models\TakenQuiz;
+
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -28,85 +17,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //***********************/
-        //******** USER *********/
-        //***********************/
+        // User
+        jsonSeeder("user", "database/data/user.json");
 
-        // Admin
-        User::create([
-            "uuid" => Str::orderedUuid()->getHex(),
-            "name" => "Blackpearl",
-            "slug" => "blackpearl",
-            "email" => "blackpearl@gmail.com",
-            "password" => bcrypt("vip123456"),
+        // Language
+        jsonSeeder("language", "database/data/language.json");
+
+        // Subject
+        jsonSeeder("subject", "database/data/subject.json");
+        jsonSeeder(
+            "subject_translation",
+            "database/data/subjectTranslation.json"
+        );
+
+        // Quiz
+        jsonSeeder("quiz", "database/data/quiz.json");
+        jsonSeeder("quiz_translation", "database/data/quizTranslation.json");
+
+        // Subject Quiz Many to Many table
+        jsonSeeder("quiz_subject", "database/data/quizSubject.json");
+
+        // Qustion
+        jsonSeeder("question", "database/data/question.json");
+        jsonSeeder(
+            "question_translation",
+            "database/data/questionTranslation.json"
+        );
+
+        // Answer
+        jsonSeeder("answer", "database/data/answer.json");
+        jsonSeeder(
+            "answer_translation",
+            "database/data/answerTranslation.json"
+        );
+
+        TakenQuiz::create([
+            "id" => 1,
+            "uuid" => "213124125ewc3",
+            "score" => 0,
+            "status" => "active",
+            "subject_quiz_id" => 1,
+            "user_id" => 14,
+            "created_at" => date("Y-m-d H:i:s"),
+            "updated_at" => date("Y-m-d H:i:s"),
         ]);
-
-        // Users
-        User::factory(10)->create([
-            "password" => bcrypt("vip123456"),
-        ]);
-
-        //***********************/
-        //******** LANG *********/
-        //***********************/
-
-        // Default languages
-        $languages = Language::insert([
-            [
-                "name" => "English",
-                "code" => "en",
-                "uuid" => Str::orderedUuid()->getHex(),
-                "created_at" => date("Y-m-d H:i:s"),
-                "updated_at" => date("Y-m-d H:i:s"),
-            ],
-            [
-                "name" => "Arabic",
-                "code" => "ar",
-                "uuid" => Str::orderedUuid()->getHex(),
-                "created_at" => date("Y-m-d H:i:s"),
-                "updated_at" => date("Y-m-d H:i:s"),
-            ],
-        ]);
-
-        //***********************/
-        //******* SUBJECT *******/
-        //***********************/
-        
-
-        jsonSeeder("subject","database/data/subject.json");
-
-        jsonSeeder("subject_translation","database/data/subjectTranslation.json");
-
-        //***********************/
-        //******** QUIZ *********/
-        //***********************/
-
-        jsonSeeder("quiz","database/data/quiz.json");
-
-        jsonSeeder("quiz_translation","database/data/quizTranslation.json");
-
-        //*******************************/
-        //******** SUBJECT QUIZ *********/
-        //*******************************/
-
-        jsonSeeder("quiz_subject","database/data/quizSubject.json");
-
-
-        //*******************************/
-        //********** QUESTION ***********/
-        //*******************************/
-
-        jsonSeeder("question","database/data/question.json");
-
-        jsonSeeder("question_translation","database/data/questionTranslation.json");
-
-        //*******************************/
-        //********** Answer ***********/
-        //*******************************/
-
-        //jsonSeeder("answer","database/data/answer.json");
-
-        //jsonSeeder("answer_translation","database/data/answerTranslation.json");
-
     }
 }
